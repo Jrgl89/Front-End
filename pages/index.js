6ef059acd0a4cb68ed91deaec983a3aaa23bb75c
Jargal <Jargal>
2025-09-02 09:35:36 +0800

    123

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default function Index() {
   return (
-   <div className="text-3xl text-red-400 ">hello</div>
+   <div className="text-3xl text-red-400 ">fsdkljfsdlkj fjdslfjdklsasdjaskld asd akldjaskdaksl</div>
   );
 }
